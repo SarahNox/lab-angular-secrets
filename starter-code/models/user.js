135c0fe3b8caee812e11f4d3f61ddf0cbb4f45ff
SarahNox <apnoeist@googlemail.com>
2017-03-03 14:52:35 +0100

    first draft

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
-const userSchema = new Schema();
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, 'The username is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'The password is required']
+  },
+  name: {
+    type: String,
+    required: [true, 'The fullname is required']
+  },
+  secret: {
+    type: String,
+    required: [true, 'The secret is required']
+},
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
